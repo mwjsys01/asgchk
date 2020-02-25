af4785adba13cd506d1ddc6aa129e091d380ee76
root <root@localhost.localdomain>
2020-02-25 01:48:05 +0000

    2nd

--- src/app/tbls/chktbl.component.ts
+++ src/app/tbls/chktbl.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
 import { MatTableDataSource } from '@angular/material/table';
+import { MatPaginator } from '@angular/material/paginator';
 import { Header,HeaderService } from '../services/header.service';
 import { Chktbl,PackService } from '../services/pack.service';
 
@@ -9,7 +10,7 @@ import { Chktbl,PackService } from '../services/pack.service';
   styleUrls: ['./chktbl.component.scss']
 })
 export class ChktblComponent implements OnInit {
-
+  @ViewChild(MatPaginator, {static: false}) paginator: MatPaginator;
   dataSource:MatTableDataSource<Chktbl>;
 
   displayedColumns = ['pacno','gcode','quant','realg','realq','result'];
@@ -23,6 +24,11 @@ export class ChktblComponent implements OnInit {
     //他コンポーネントからの更新
     this.packservice.observe.subscribe(() => this.updateData());
   }
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+    // this.dataSource.sort = this.sort;
+  }
+
   checkPac(i: number): void {
     this.packservice.setOk(i);
     this.updateData();
@@ -30,6 +36,7 @@ export class ChktblComponent implements OnInit {
   updateData(): void {
     //tableのデータソース更新
     this.dataSource= new MatTableDataSource<Chktbl>(this.packservice.getChktbl());
+    this.dataSource.paginator = this.paginator;
     console.log(this.packservice.getChktbl());
   }
 }
